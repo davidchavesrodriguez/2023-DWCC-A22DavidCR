203a7597bae3d9cd9a7ff2dc360b0d6efb0c9b86
David Chaves Rodríguez <A22DavidCR@iessanclemente.net>
2023-10-03 09:19:41 +0200

    algo de datas

--- datas/datas3.js
+++ datas/datas3.js
@@ -1,9 +1,14 @@
 // 3. Calcula o número de días que pasaron dende o 25 de xullo de 2000 ata hoxe.
 const primeiraFecha = new Date(2000, 6, 25).valueOf();
-console.log(primeiraFecha);
+// console.log(primeiraFecha);
 
 const actual = new Date().valueOf();
-console.log(actual);
+// console.log(actual);
 
 const milisegundos = actual - primeiraFecha;
-console.log(milisegundos / (1000 * 60 * 60 * 24));
+console.log(
+  `Dende o 25 de xullo do 2000 pasaron ${(
+    milisegundos /
+    (1000 * 60 * 60 * 24)
+  ).toFixed(3)} milisegundos`
+);
--- datas/datas6.js
+++ datas/datas6.js
@@ -1,3 +1,5 @@
 // 6. Crea unha función que reciba unha data como parámetro data e devolva o número de días que pasaron dende que comezou o ano.
-
-function diasDendeOInicio(data) {}
+function diasDendeOInicio(data) {
+  console.log('Hola, Isamel');
+}
+diasDendeOInicio();
