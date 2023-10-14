a6b5e5acbbe101799845634e277f789c481a1652
David Chaves Rodríguez <A22DavidCR@iessanclemente.net>
2023-10-14 16:20:24 +0200

    commitCasa

---
+++ 7- destObjetos/destObjetos5.js
@@ -0,0 +1,27 @@
+// 1. O seguinte mapa almacena información dos eventos ocorridos durante un partido
+// indicando o minuto no que se produciron:
+const gameEvents = new Map([
+  [17, 'GOAL'],
+  [36, 'Substitution'],
+  [47, 'GOAL'],
+  [61, 'Substitution'],
+  [64, 'Yellow card'],
+  [69, 'Red card'],
+  [70, 'Substitution'],
+  [72, 'Substitution'],
+  [76, 'GOAL'],
+  [80, 'GOAL'],
+  [92, 'Yellow card'],
+]);
+// a. Crea un novo array chamado eventos que almacene os diferentes eventos
+// (non o minuto) ocorridos durante o partido (sen que haxa duplicados).
+const eventos = [...new Set(gameEvents.values())];
+console.log(eventos);
+
+// b. Recorre con un bucle o mapa gameEvents e mostra información de cada
+// evento, indicando se ocorreu na primeira metade ou na segunda metade do
+// partido, por exemplo: [PRIMEIRA PARTE] 17: GOAL.
+for (const [minuto, evento] of gameEvents) {
+  const mitad = minuto <= 45 ? 'Primeira' : 'Segunda';
+  console.log(`[${mitad} mitad] ${minuto}: ${evento}`);
+}
