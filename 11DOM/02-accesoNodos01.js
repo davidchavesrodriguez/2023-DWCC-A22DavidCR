59066819f0931e6fe24812fb353df5a07a444d8e
David Chaves Rodríguez <A22DavidCR@iessanclemente.net>
2023-10-31 10:25:30 +0100

    js

---
+++ 11DOM/02-accesoNodos01.js
@@ -0,0 +1,17 @@
+// A táboa con id="age-table".
+const idTable= document.getElementById("age-table");
+
+// Todos os elementos label dentro da táboa (debería haber 3).
+const allTableElements= document.querySelectorAll("table label");
+
+// O primeiro td da táboa.
+const firstTableTd= document.querySelector("table td");
+
+// O form con name="search".
+const formName= document.getElementsByName("search");
+
+// O primeiro input do formulario anterior.
+const firstFormNameInput= document.querySelector("input");
+
+// O último input do formulario do exercicio anterior.
+const lastFormNameInput= document.querySelectorAll[-1]("input");
