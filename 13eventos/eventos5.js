e7ec2c6e086127e22ed1adbf289beb84537c2b91
David Chaves Rodríguez <A22DavidCR@iessanclemente.net>
2023-11-13 21:06:03 +0100

    mouseCousas

--- 13eventos/eventos5.js
+++ 13eventos/eventos5.js
@@ -3,3 +3,48 @@
 // mouseover, mouseout, dragstart, drag, dragend. Cada vez que suceda un destes
 // eventos mostra por consola unha mensaxe informando do evento que sucedeu.
 // Proba os diferentes eventos e observa a orde na que suceden.
+let imgProba = document.getElementById('imgEx5');
+
+imgProba.addEventListener('click', () => {
+  console.log('Click');
+});
+
+imgProba.addEventListener('dblclick', () => {
+  console.log('Doble Click');
+});
+
+imgProba.addEventListener('contextmenu', () => {
+  console.log('Context Menu');
+});
+
+imgProba.addEventListener('mousedown', () => {
+  console.log('Mouse Down');
+});
+
+imgProba.addEventListener('mouseup', () => {
+  console.log('Mouse Up');
+});
+
+imgProba.addEventListener('wheel', () => {
+  console.log('Wheel');
+});
+
+imgProba.addEventListener('mouseover', () => {
+  console.log('Mouse Over');
+});
+
+imgProba.addEventListener('mouseout', () => {
+  console.log('Mouse Out');
+});
+
+imgProba.addEventListener('dragstart', () => {
+  console.log('Drag Start');
+});
+
+imgProba.addEventListener('drag', () => {
+  console.log('Drag');
+});
+
+imgProba.addEventListener('dragend', () => {
+  console.log('Drag End');
+});
