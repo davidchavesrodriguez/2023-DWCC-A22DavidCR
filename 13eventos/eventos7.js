1ace5989682d014753e7192ae68725760e23650e
David Chaves Rodríguez <A22DavidCR@iessanclemente.net>
2023-11-16 09:08:41 +0100

    gpt

--- 13eventos/eventos7.js
+++ 13eventos/eventos7.js
@@ -4,9 +4,14 @@
 // Ademais, cando a caixa de texto perda o foco e estea baleira, debe escribirse por
 // consola unha mensaxe informando da situación.
 let texto = document.getElementById('textBox');
+
 texto.addEventListener('focus', function () {
-  texto.style.border = '5px solid red';
+  texto.style.border = '3px solid green';
 });
+
 texto.addEventListener('blur', function () {
-  texto.style.border = '';
+  if (texto.value.trim() === '') {
+    console.log('La caja de texto está vacía.');
+  }
+  texto.style.border = '2px solid black';
 });
--- 13eventos/eventos8.js
+++ 13eventos/eventos8.js
@@ -2,3 +2,25 @@
 // select, radiobutton, checkbox, área de texto. Escribe o código JavaScript necesario
 // para mostrar por consola todos os valores dos campos do formulario cando este se
 // envíe.
+
+// Obtener el formulario por su ID
+const formulario = document.getElementById('miFormulario');
+
+// Agregar un evento al formulario para capturar el envío
+formulario.addEventListener('submit', function (event) {
+  // Prevenir el comportamiento por defecto del formulario (evitar que se recargue la página)
+  event.preventDefault();
+
+  // Obtener todos los elementos del formulario
+  const elementos = formulario.elements;
+
+  // Iterar sobre los elementos y mostrar sus valores por consola
+  for (let i = 0; i < elementos.length; i++) {
+    const elemento = elementos[i];
+
+    // Verificar si el elemento tiene un valor
+    if (elemento.value.trim() !== '') {
+      console.log(`${elemento.name}: ${elemento.value}`);
+    }
+  }
+});
