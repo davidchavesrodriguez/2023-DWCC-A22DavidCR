783d1587f94d631d2e68b7948e3cb5e5596c82d4
David Chaves Rodríguez <A22DavidCR@iessanclemente.net>
2024-01-11 11:15:09 +0100

    isamel

---
+++ segundoTrimestre/promesas/promesas5.js
@@ -0,0 +1,4 @@
+// No apartado “Funcións de callback personalizadas” móstrase un exemplo 
+// de execución de código asíncrono usando funcións de callback. 
+// Reescribe dito código para que a función createPosts() devolva unha 
+// promesa e fai que o resultado sexa equivalente.
