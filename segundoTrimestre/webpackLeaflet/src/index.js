5478aa4765745a3b687bb197cc4f2130730d1bca
David Chaves Rodríguez <A22DavidCR@iessanclemente.net>
2024-02-05 17:59:39 +0100

    checar image

--- segundoTrimestre/webpackLeaflet/src/index.js
+++ segundoTrimestre/webpackLeaflet/src/index.js
@@ -1,9 +1,9 @@
 // index.js
 import "./css/style.css";
-import L from "leaflet";
+import L, { imageOverlay } from "leaflet";
 import "leaflet/dist/leaflet.css";
 import "@geoapify/leaflet-address-search-plugin/dist/L.Control.GeoapifyAddressSearch.min.css";
-import '@geoapify/leaflet-address-search-plugin';
+import "@geoapify/leaflet-address-search-plugin";
 
 import markerIcon from "../node_modules/leaflet/dist/images/marker-icon.png";
 L.Marker.prototype.setIcon(L.icon({ iconUrl: markerIcon }));
@@ -11,17 +11,18 @@ L.Marker.prototype.setIcon(L.icon({ iconUrl: markerIcon }));
 const map = L.map("map", {
   center: L.latLng(42.64585380457401, -8.885536531719335),
   zoom: 14,
-  zoomControl: false
+  zoomControl: false,
 });
 
-L.control.zoom({
-  position: 'bottomright'
-}).addTo(map);
-
+L.control
+  .zoom({
+    position: "bottomright",
+  })
+  .addTo(map);
 
 const key = "yQjDN2uAGBZeUGhEWI26";
 
-/*Definimos tiles (e tiles de probas)*/
+/*Definimos tiles*/
 
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
@@ -29,47 +30,46 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
 
-// L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-// 	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-// }).addTo(map);
-
-// L.tileLayer('https://map1.vis.earthdata.nasa.gov/wmts-webmerc/VIIRS_CityLights_2012/default/{time}/{tilematrixset}{maxZoom}/{z}/{y}/{x}.{format}', {
-// 	attribution: 'Imagery provided by services from the Global Imagery Browse Services (GIBS), operated by the NASA/GSFC/Earth Science Data and Information System (<a href="https://earthdata.nasa.gov">ESDIS</a>) with funding provided by NASA/HQ.',
-// 	bounds: [[-85.0511287776, -179.999999975], [85.0511287776, 179.999999975]],
-// 	minZoom: 1,
-// 	maxZoom: 8,
-// 	format: 'jpg',
-// 	time: '',
-// 	tilematrixset: 'GoogleMapsCompatible_Level'
-// }).addTo(map);
-
-// L.tileLayer(mapURL, {
-//   attribution: 'Powered by Geoapify | © OpenMapTiles © OpenStreetMap contributors',
-//   apiKey: myAPIKey,
-//   mapStyle: "osm-bright-smooth", // More map styles on https://apidocs.geoapify.com/docs/maps/map-tiles/
-//   maxZoom: 20
-// }).addTo(map);
-
-
 //Marcador inicial
-let markerAgora = L.marker([42.64585380457401, -8.885536531719335]).addTo(map);
+L.marker([42.64585380457401, -8.885536531719335]).addTo(map);
 
+let coordenadas;
+// Añadir marcador con nome
+map.on("click", function (event) {
+  let inputMarker = document.getElementById("inputMarker");
+  let latitud = event.latlng.lat;
+  let longitud = event.latlng.lng;
+  coordenadas = [latitud, longitud];
 
-//Geocoding
-var myAPIKey = "99925a27499641468c5be50557317a82"; 
+  let imageOverlay;
+  if (imageOverlay.value) {
+    imageOverlay = L.imageOverlay(
+      "",
+      [
+        [event.latlng.lat, event.latlng.lng], // Esquina superior izquierda
+        [event.latlng.lat + 0.001, event.latlng.lng + 0.001], // Esquina inferior derecha (ajusta según tu necesidad)
+      ],
+      {
+        opacity: 0.8,
+        errorOverlayUrl: "",
+        alt: "Aquí?",
+        interactive: true,
+      }
+    ).addTo(map);
+  }
 
-const addressSearchControl = L.control.addressSearch(myAPIKey, {
-  position: 'topleft',
-  resultCallback: (selectedAddress) => {
-    console.log(selectedAddress);
-  },
-  suggestionsCallback: (suggestions) => {
-    console.log(suggestions);
+  inputMarker.classList.remove("hidden");
+  inputMarker.focus();
+});
+
+// ao pulsar enter
+inputMarker.addEventListener("keypress", (eventKey) => {
+  if (eventKey.key === "Enter") {
+    let newMarker = L.marker([coordenadas[0], coordenadas[1]]);
+    inputMarker.blur();
+    inputMarker.classList.add("hidden");
+
+    newMarker.bindPopup(inputMarker.value);
+    newMarker.addTo(map);
   }
 });
-map.addControl(addressSearchControl);
-
-
-
-
-console.log("Hula");
