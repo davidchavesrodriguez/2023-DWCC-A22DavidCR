944cfc372272a4982e252a098a5137cd7f99985d
David Chaves Rodríguez <A22DavidCR@iessanclemente.net>
2024-02-20 10:24:43 +0100

    vite

---
+++ segundoTrimestre/vue/probaClase/viteProject/src/components/OlaMundo.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+    data() {
+        return {
+            message: 'Ola mundo',
+            addition: "!!!",
+            added: false
+        };
+    },
+    methods: {
+        toggleCase() {
+            this.message = this.added ? this.message.toUpperCase() :
+                this.message.toLowerCase();
+            this.added = !this.added;
+        },
+        addCharacter(){
+            this.message+= "!";
+        },
+    }
+};
+</script>
+<template>
+    <p>{{ message }}</p>
+    <button type="button" @click="toggleCase">MAYUS/minus</button>
+    <button type="button" @click="addCharacter">!!!</button>
+</template>
+<style></style>
