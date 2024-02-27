bd619afe9ac2e71429cd64aee6414571ac69eb74
David Chaves Rodríguez <A22DavidCR@iessanclemente.net>
2024-02-27 10:25:23 +0100

    form

---
+++ segundoTrimestre/vue/vite/vmodel/vmodel1/src/components/Component.vue
@@ -0,0 +1,14 @@
+<script></script>
+<template>
+    <ul>
+        <li>
+            <button type="button">Baixo</button>
+        </li>
+        <li>
+            <button type="button">Medio</button>
+        </li>
+        <li>
+            <button type="button">Alto</button>
+        </li>
+    </ul>
+</template>
