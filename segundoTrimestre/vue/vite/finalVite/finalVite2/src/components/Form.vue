22029d7fb4a2ab24a6e594cfc49a48cca4f6ee34
David Chaves Rodríguez <A22DavidCR@iessanclemente.net>
2024-03-07 11:15:20 +0100

    tocou

--- segundoTrimestre/vue/vite/finalVite/finalVite2/src/components/Form.vue
+++ segundoTrimestre/vue/vite/finalVite/finalVite2/src/components/Form.vue
@@ -1,12 +1,20 @@
 <template>
     <form action="#">
-        <label for="formName" class="form-label">Name:</label>
-        <input type="text" name="formName" class="form-text">
-        <label for="formLastname" class="form-label">Lastname:</label>
-        <input type="text" name="formLastname" class="form-text">
-        <button class="btn btn-primary">Add</button>
-        <button class="btn btn-warning">Patch</button>
-        <button class="btn btn-danger">Delete</button>
+        <div class="row">
+            <label for="formName" class="form-label">Name:</label>
+            <input type="text" name="formName" class="form-text">
+        </div>
+
+        <div class="row"> <label for="formLastname" class="form-label">Lastname:</label>
+            <input type="text" name="formLastname" class="form-text">
+        </div>
+        <div class="row">
+            <button class="btn btn-primary">Add</button>
+            <button class="btn btn-warning">Patch</button>
+            <button class="btn btn-danger">Delete</button>
+        </div>
+
+
     </form>
 </template>
 <script>
